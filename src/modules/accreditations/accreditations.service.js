86e4cbb47b8ad0d98fc00d473bdc712c8ec754de
MaxPce <max.ponce@tecsup.edu.pe>
2026-07-11 13:08:08 -0500

    11-7-2

--- src/modules/accreditations/accreditations.service.js
+++ src/modules/accreditations/accreditations.service.js
@@ -2,6 +2,8 @@
 const pool = require("../../config/db");
 const AppError = require("../../utils/AppError");
 
+const PHOTOS_BASE_URL = process.env.PHOTOS_BASE_URL || "https://master.hayllis.com/writable/uploads/";
+
 const BASE_SELECT = `
   SELECT a.idacreditation, a.idcompany, a.idevent, a.idsport, a.idinstitution,
          a.tregister, a.checkdoc, a.hosting,
@@ -9,7 +11,8 @@ const BASE_SELECT = `
          p.firstname, p.lastname, p.surname,
          docm.name_es AS doctype_name,
          rolm.name_es AS role_name,
-         rolm.param2  AS role_color
+         rolm.param2  AS role_color,
+         ph.ruta      AS photo_ruta
   FROM accreditation a
   INNER JOIN person p
     ON p.idcompany = a.idcompany AND p.idperson = a.idperson
@@ -17,8 +20,9 @@ const BASE_SELECT = `
     ON docm.idcompany = a.idcompany AND docm.idmaster = 4 AND docm.iddetails = p.doctype
   LEFT JOIN master_details rolm
     ON rolm.idcompany = a.idcompany AND rolm.idmaster = 19 AND rolm.iddetails = a.tregister
+  LEFT JOIN photos ph
+    ON ph.idcompany = a.idcompany AND ph.idphoto = a.idphoto AND ph.mstatus = 1
 `;
-
 
 function mapAccreditation(row) {
   return {
@@ -26,7 +30,7 @@ function mapAccreditation(row) {
     idevent:        row.idevent,
     idsport:        row.idsport,
     idinstitution:  row.idinstitution,
-    hosting:        row.hosting === 1 || row.hosting === true, // boolean
+    hosting:        row.hosting === 1 || row.hosting === true,
     role: {
       code:  row.tregister,
       name:  row.role_name || row.tregister,
@@ -39,10 +43,10 @@ function mapAccreditation(row) {
       doctype:     row.doctype,
       doctypeName: row.doctype_name,
       docnumber:   row.docnumber,
+      photoUrl:    row.photo_ruta ? `${PHOTOS_BASE_URL}${row.photo_ruta}` : null,
     },
   };
 }
-
 
 async function findByQr({ idcompany, idevent, idacreditation }) {
   const [rows] = await pool.query(
